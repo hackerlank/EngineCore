e4ff146d2c86dc655eed83592b02d762f73ff799
Oliver Walter <oliver.walter@student.kit.edu>
2016-06-27 15:45:33 +0200

    update

--- renderoffscreen.h
+++ renderoffscreen.h
@@ -1,9 +1,14 @@
 #ifndef RENDEROFFSCREEN_H
 #define RENDEROFFSCREEN_H
 
+#include <QOffscreenSurface>
+#include <QOpenGLFunctions_3_3_Core>
+#include <QOpenGLFramebufferObject>
 
-class RenderOffScreen
+class RenderOffScreen : public QOffscreenSurface , public QOpenGLFunctions_3_3_Core
 {
+    QOpenGLFramebufferObject * frameBuffer;
+
 public:
     RenderOffScreen();
 };
